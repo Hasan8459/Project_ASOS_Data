bb38d061059b84d39dc977a4420a099d6c9e6127
Hasan Patel <Hasan8459@users.noreply.github.com>
2023-12-02 21:11:39 +0530

    Some New

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const jsonServer = require.("json-server")   //Importing json-server library
-const server = jsonServer.create()
-const router = jsonServer.router("db.json")
-const middlewares = jsonServer.default()
+const jsonServer = require("json-server");   //Importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.default();
 const port = process.env.PORT || 8000;    //choose port from here like 8000, 8080, 5000
 
 server.use(middlewares);
